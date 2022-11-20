080c063a4d10a3deab0c7fec788bc730367b9a24
ht <ht@lq.com>
2022-11-20 22:00:57 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,9 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hello git5!");
-        System.out.println("master");
-        System.out.println("hot-fix");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+
     }
 }
